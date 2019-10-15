190f974245bf52e772faf69f34ed42019352d930
Raúl <varela.raul@ymail.com>
2019-10-15 09:35:17 -0300

    Ejemplos en Clase

---
+++ tpa-bootstrap/src/main/java/com/tpa/css/TpaBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.tpa.css;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaBootstrapApplication.class, args);
+	}
+
+}
