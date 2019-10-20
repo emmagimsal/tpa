2654602cbdd062a2bb18d9d0d0e9e279b09e7dcf
Raúl <varela.raul@ymail.com>
2019-10-20 09:51:55 -0300

    keycloack

---
+++ keyCloack/src/main/java/com/tpa/keyCloack/KeyCloackApplication.java
@@ -0,0 +1,13 @@
+package com.tpa.keyCloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyCloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyCloackApplication.class, args);
+	}
+
+}
---
+++ keyCloack/src/main/java/com/tpa/keyCloack/controller/CustomerDAO.java
@@ -0,0 +1,7 @@
+package com.tpa.keyCloack.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerDAO extends CrudRepository<Customer, Long> {
+
+}
